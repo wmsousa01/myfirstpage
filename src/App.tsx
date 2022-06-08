69e3b42c34e6d04a15cce73a97ae65060ebfad77
Sousa <willian.marinho@sealedair.com>
2022-06-08 09:32:31 -0300

    Texto

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          E ai<code>Seu Otário</code> hehehe.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Powered by Tião
         </a>
       </header>
     </div>
